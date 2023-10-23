d68e2d9b316a18601fac37b402aba9b15c04852c
João Oliveira <jaoovit@gmail.com>
2023-10-23 14:33:20 +0100

    create a logic to select a image using the parameter age and sex

--- script.js
+++ script.js
@@ -7,6 +7,38 @@ function check() {
     if (fyear.value.length == 0 || fyear.value > year) {
         window.alert('[ERRO] Check the date and try again')
     } else {
-        
+        let fsex = document.getElementsByName('radsex')
+        let yearsold = year - Number(fyear.value)
+        let gender = ''
+        let img =document.createElement('img')
+        img.setAttribute('id', 'photo')
+        if(fsex[0].checked) {
+            gender = 'man'
+            if (yearsold >= 0 && yearsold < 10) {
+
+            } else if (yearsold < 21) {
+
+            } else if (yearsold < 50) {
+
+            } else {
+
+            }
+
+        } else if (fsex[1].checked) {
+            gender = 'woman'
+            if (yearsold >= 0 && yearsold < 10) {
+
+            } else if (yearsold < 21) {
+
+            } else if (yearsold < 50) {
+
+            } else {
+                
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `We detected a ${gender} with ${yearsold} years old.`
+
+    
     }
 }
